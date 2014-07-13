b1bd8ff9f324569ed8b57987db022d86e8d70cb9
Paulo L F Casaretto <pcasaretto@gmail.com>
2014-07-13 00:39:27 -0300

    Add code and image for plot 4

---
+++ plot4.R
@@ -0,0 +1,13 @@
+source("data_load.R")
+png("plot4.png",width=480,height=480,units="px")
+par(mfrow = c(2,2))
+with(data,{
+  plot(Date_and_Time, Global_active_power, type = "l", xlab="", ylab="Global Active Power")
+  plot(Date_and_Time, Voltage, type = "l", xlab="datetime")
+  plot(Date_and_Time, Sub_metering_1, col = "black", type = "l", xlab="", ylab="Energy sub metering")
+  lines(Date_and_Time, Sub_metering_2, col = "red", type = "l", xlab="", ylab="Energy sub metering")
+  lines(Date_and_Time, Sub_metering_3, col = "blue", type = "l", xlab="", ylab="Energy sub metering")
+  legend("topright", bty="n", lty=1, col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+  plot(Date_and_Time, Global_reactive_power, type = "l", xlab="datetime")
+})
+dev.off()
